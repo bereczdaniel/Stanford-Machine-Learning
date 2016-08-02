40a8a63e6253d5c1cba4cdf8394485938cb7eddb
bdaniel <berecz.danie@nextent.hu>
2016-08-02 23:26:44 +0200

    Everything that left from exercise 5

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -17,6 +17,9 @@ function [error_train, error_val] = ...
 % Number of training examples
 m = size(X, 1);
 
+initial_theta_train = ones(size(X)(2), 1);
+initial_theta_cv = ones(size(X)(2), 1);
+
 % You need to return these values correctly
 error_train = zeros(m, 1);
 error_val   = zeros(m, 1);
@@ -53,6 +56,13 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+
+
+for i=1:m
+	theta = trainLinearReg(X(1:i, :), y(1:i), lambda)
+	error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+	error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end;
 
 
 
