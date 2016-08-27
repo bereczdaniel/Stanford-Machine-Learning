7e6dfc6bf8d02ea635dd9bec0a5664ae487e3505
bdaniel <berecz.danie@nextent.hu>
2016-08-27 11:24:20 +0200

    refactor

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -6,19 +6,19 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-n = size(X, 2)
+n = size(X, 2);
 temp = zeros(n, 1);
 
 for i = 1:num_iters
+
+
   for j = 1:n
     temp(j) = theta(j) - alpha / m * ((theta' * X' * X(:, j)) - sum(y .* X(:, j)));
-    																X' * y
   end;
-
-  theta = theta - alpha / m * (theta' * X') 
 
   theta = temp;
   J_history(i) = computeCostMulti(X, y, theta);
     
 end;
+
 end
